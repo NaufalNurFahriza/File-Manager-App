31f5039d77b0f3d81682a57bb10b0a4ce9fc678b
Naufalnurfahriza <123190024@student.upnyk.ac.id>
2024-03-23 14:31:55 +0700

    update convert file image to PDF (still error)

--- src/screen/ConvertFile.js
+++ src/screen/ConvertFile.js
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
   ScrollView,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import RNImageToPdf from 'react-native-image-to-pdf'; // Import RNImageToPdf module
+import {launchImageLibrary} from 'react-native-image-picker';
 
-export default function ConvertFile({navigation}) {
+export default function ConvertFile({ navigation }) {
+  const [imageData, setImageData] = useState(null);
+
+  const convertToPDF = async () => {
+    try {
+      const { width, height } = Dimensions.get('window'); // Get screen dimensions
+      const options = {
+        imagePaths: [imageData.uri], // Pass the image path to convert
+        name: 'ConvertedPDF', // PDF file name
+        maxSize: {
+          width: 900,
+          height: Math.round((height / width) * 900), // Calculate maximum image dimension
+        },
+        quality: 0.7, // optional compression parameter
+      };
+      const pdf = await RNImageToPdf.createPDFbyImages(options);
+      
+      console.log(pdf.filePath); // Log the PDF file path
+    } catch(e) {
+      console.log(e);
+    }
+  };
+
+
+  const handleImageSelection = (image) => {
+    setImageData(image);
+  };
+
+  const openImageLibraryPDF = () => {
+    const options = {
+      mediaType: 'photo',
+      maxWidth: 1024,
+      maxHeight: 1024,
+      quality: 1,
+      includeBase64: false,
+    };
+
+    launchImageLibrary(options, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.errorCode) {
+        console.log(
+          'ImagePicker Error: ',
+          response.errorCode,
+          response.errorMessage,
+        );
+      } else {
+        console.log('ImagePicker Response: ', response);
+        handleImageSelection(response.assets[0]);
+      }
+    });
+  };
+
   return (
     <View className="flex-1 bg-slate-100">
       <View className="w-full h-16 bg-white flex-row self-center items-center pl-3 shadow-md">
@@ -24,48 +79,40 @@ export default function ConvertFile({navigation}) {
         contentContainerStyle={{paddingBottom: 10}}>
         <View className="px-6 py-6 items-center">
           <Text className="text-stone-900 font-semibold text-lg text-center pt-10 pb-8">
-            Choose Files
+            Choose Image from Gallery
           </Text>
 
-          <TouchableOpacity className="w-56 rounded-lg py-3 border-blue-700 border-2 justify-center items-center">
+          <TouchableOpacity
+            onPress={openImageLibraryPDF}
+            className="w-56 rounded-lg py-3 border-blue-700 border-2 justify-center items-center">
             <Text className="text-blue-700 font-semibold text-lg">
               Select File
             </Text>
           </TouchableOpacity>
 
-          <View className="py-5">
-            <Text className="text-stone-900 font-semibold text-base text-center">
-              Or
-            </Text>
-          </View>
-
-          <TouchableOpacity className="w-56 rounded-lg py-3 border-blue-700 border-2 justify-center items-center">
-            <Text className="text-blue-700 font-semibold text-lg">
-              Choose from Gallery
-            </Text>
-          </TouchableOpacity>
-
-          <View className="bg-white px-4 py-6 w-80 my-9">
-            <View className="flex-row justify-between py-3">
-              <View className="flex-row items-center">
-                <MaterialIcons
-                  name="insert-drive-file"
-                  size={24}
-                  color="gray"
-                />
-                <Text className="pl-5 font-medium text-base text-gray-600">
-                  Catatan.txt
-                </Text>
+          {imageData && (
+            <View className="bg-white px-4 py-6 w-80 my-9">
+              <View className="flex-row justify-between py-3">
+                <View className="flex-row items-center">
+                  <MaterialIcons
+                    name="image"
+                    size={24}
+                    color="#87CEEB"
+                  />
+                  <Text className="pl-5 font-medium text-base text-gray-600">
+                    {imageData.fileName.length > 20 ? `${imageData.fileName.substring(0, 20)}...` : imageData.fileName}
+                  </Text>
+                </View>
+                <TouchableOpacity onPress={() => setImageData(null)}>
+                  <MaterialIcons name="close" size={24} color="gray" />
+                </TouchableOpacity>
               </View>
-              <TouchableOpacity>
-                <MaterialIcons name="close" size={24} color="gray" />
-              </TouchableOpacity>
             </View>
-          </View>
+          )}
 
           <TouchableOpacity
             className="rounded-lg my-6 py-3 bg-red-700 justify-center items-center w-80"
-            onPress={() => navigation.navigate('Home')}>
+            onPress={convertToPDF}>
             <Text className="text-white font-semibold text-lg">
               Convert to PDF
             </Text>
